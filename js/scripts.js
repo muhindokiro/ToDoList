ed7790083fac5b2bd60b934be5c41522e3792e31
muhindokiro <blaize1143@gmail.com>
2020-02-02 21:24:06 +0300

    create if statements for variables created ie hour and minutes for time to be displayed in 24hr clock system,,create new var myclock to target id used in html text clockDisplay,set var myclock to display time hours and minutes

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,21 @@ function renderTime() {
     var currentTime = new Date();
     var h = currentTime.getHours();
     var m = currentTime.getMinutes();
+    if (h == 24) {
+        h = 0;
+    } else if (h > 12) {
+        h = h - 0;
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    var myClock = document.getElementById("clockDisplay");
+    myClock.textContent = "" + h + ":" + m;
 
+    setTimeout("renderTime()", 1000);
 
 }
 renderTime();
