e445e3f2c67f3388bf39de3e2a654ece99d5c63b
muhindokiro <blaize1143@gmail.com>
2020-02-02 21:15:04 +0300

    create function rendertime, creat variables currentime,hoirs,minutes to trigger the function rendertime

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ const input = document.getElementById("input");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const myClock = document.getElementById("clockDisplay");
+
+//Time of TO Do List
+function renderTime() {
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var m = currentTime.getMinutes();
+
+
+}
+renderTime();
